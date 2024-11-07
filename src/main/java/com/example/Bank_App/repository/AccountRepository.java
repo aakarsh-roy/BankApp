b0901f51ae6195d4da1da2dbf15da811182a552c
Utkarsh Roy <123242625+im-ukr@users.noreply.github.com>
2024-11-07 16:31:48 +0530

    first commit

---
+++ src/main/java/com/example/Bank_App/repository/AccountRepository.java
@@ -0,0 +1,10 @@
+package com.example.Bank_App.repository;
+
+import com.example.Bank_App.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByUsername(String username);
+}
---
+++ src/main/java/com/example/Bank_App/repository/TransactionRepository.java
@@ -0,0 +1,10 @@
+package com.example.Bank_App.repository;
+
+import com.example.Bank_App.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByAccountId(Long accountId);
+}
